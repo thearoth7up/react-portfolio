0fc328d1b3650fa3dc58e41f2a9060b73a9a1239
thearoth7up <oumkunthearothup@gmail.com>
2025-09-09 22:28:58 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path'
 export default defineConfig({
   
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/thearoth-portfolio",
+  base: process.env.VITE_BASE_PATH || "/react-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), 
